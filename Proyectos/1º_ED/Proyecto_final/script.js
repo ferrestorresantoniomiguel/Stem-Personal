121309eb94ac9226d8c3301b0ed0f151bd732817
ferrestorresantoniomiguel <ferrestorresantoniomiguel001.alu@stemgranada.com>
2025-11-26 17:19:39 +0100

    subido proyecto final de ed

---
+++ Proyectos/1º_ED/Proyecto_final/script.js
@@ -0,0 +1,59 @@
+//Funciones
+function eleccionMaquina() {
+  //Creamos un número random para que no sobrepase el nº de opciones
+  let indiceRandom = Math.floor(Math.random() * OPCIONES.length);
+  //Devolvemos la opción
+  return OPCIONES[indiceRandom];
+}
+
+function determinarGanador(jugadorEleccion, maquinaEleccion) {
+  let resultadoRonda;
+
+  //Ambos sacan la misma elección
+  if (jugadorEleccion === maquinaEleccion) {
+    resultadoRonda = "Empate entre los jugadores";
+  } //Jugador gana
+  else if (
+    (jugadorEleccion === "piedra" && maquinaEleccion === "tijera") ||
+    (jugadorEleccion === "papel" && maquinaEleccion === "piedra") ||
+    (jugadorEleccion === "tijera" && maquinaEleccion === "papel")
+  ) {
+    jugadorVictoria++;
+    resultadoRonda = "Has ganado";
+  } else {
+    //Maquina Gana
+    maquinaVictoria++;
+    resultadoRonda = "Gana la consola";
+  } //Devuelve el resultado de esa ronda
+  return resultadoRonda;
+}
+
+//-------------Main------------------------
+
+//Principales variables o variables globales
+let jugadorVictoria = 0;
+let maquinaVictoria = 0;
+const OPCIONES = ["piedra", "papel", "tijera"];
+
+// Este es el juego
+function juego(eleccionJugador) {
+  //Llamamos a la función y le damos un valor a la elección de la máquina
+  let eleccionMaquinaActual = eleccionMaquina();
+  //Llamamos a la función y le determinamos quien ha ganado
+  let resultado = determinarGanador(eleccionJugador, eleccionMaquinaActual);
+
+  //Actualiza las elecciones de los jugadores
+  document.getElementById("user-choice").innerText =
+    "Tu elección: " + eleccionJugador.toUpperCase();
+  document.getElementById("computer-choice").innerText =
+    "Elección de la consola: " + eleccionMaquinaActual.toUpperCase();
+
+    //Actualiza el resultado de la ronda actual
+  document.getElementById("winner").innerText = "Resultado: " + resultado;
+
+  //Actualiza el marcador de las victorias de cada jugador
+  document.getElementById("user-wins").innerText =
+    "Victorias del jugador: " + jugadorVictoria;
+  document.getElementById("computer-wins").innerText =
+    "Victorias de la computadora: " + maquinaVictoria;
+}
