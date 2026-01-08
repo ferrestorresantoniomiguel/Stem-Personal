83f2ca22fb4c309a48e3238df45e0c7d976ca764
ferrestorresantoniomiguel <ferrestorresantoniomiguel001.alu@stemgranada.com>
2026-01-08 17:16:55 +0100

    haciendo ej5 JAVA

--- 1DAM-Programacion/Java/UD1/R2/ejercicio5.java
+++ 1DAM-Programacion/Java/UD1/R2/ejercicio5.java
@@ -1,3 +1,14 @@
+import java.util.Scanner;
+
 public class ejercicio5 {
+    public static void main(String[] args) {
+    Scanner scanner1 = new Scanner(System.in);
     
+    System.out.print("Introduzca su peso: ");
+    String pesoEnLaTierra = scanner1.nextLine();
+
+    
+
+
+    }
 }
