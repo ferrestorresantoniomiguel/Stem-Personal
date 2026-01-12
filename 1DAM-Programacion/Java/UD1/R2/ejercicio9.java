de30d961759d7cd1c2dc2d2155a62cb6de194142
ferrestorresantoniomiguel <ferrestorresantoniomiguel001.alu@stemgranada.com>
2026-01-12 12:59:08 +0100

    hecho ej9

--- 1DAM-Programacion/Java/UD1/R2/ejercicio9.java
+++ 1DAM-Programacion/Java/UD1/R2/ejercicio9.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class ejercicio9 {
-    
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("¿Cuántos caramelos tienes? ");
+        int numeroDeCaramelos = teclado.nextInt();
+
+        System.out.print("¿Cuántos niños hay? ");
+        int numeroDeNinios = teclado.nextInt();
+        
+        int carameloPorNinio = numeroDeCaramelos / numeroDeNinios;
+        int restoDeCaramelos = numeroDeCaramelos % numeroDeNinios;
+        System.out.println("A cada niño le tocan " + carameloPorNinio + " caramelos y han sobrado " + restoDeCaramelos);
+
+        teclado.close();
+    }
 }
