dd4c8b03fd8d1d53e79f5e99c942e7d37e9349c3
ferrestorresantoniomiguel <ferrestorresantoniomiguel001.alu@stemgranada.com>
2026-01-12 13:03:37 +0100

    realizado ej5 R2

--- 1DAM-Programacion/Java/UD1/R3/ejercicio5.java
+++ 1DAM-Programacion/Java/UD1/R3/ejercicio5.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
+
 public class ejercicio5 {
-    
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        // 1. Pedir el número
+        System.out.print("Introduzca el numero: ");
+        int numero = teclado.nextInt();
+
+        // 2. Lógica booleana
+        // En Java, el tipo de dato para verdadero/falso se escribe 'boolean' completo
+        boolean estaEntre10y20 = (numero >= 10 && numero <= 20);
+
+        // 3. Estructura condicional
+        if (estaEntre10y20) {
+            System.out.println("El número está entre 10 y 20");
+        } else {
+            System.out.println("El número NO está entre 10 y 20");
+        }
+
+        teclado.close();
+    }
+
 }
