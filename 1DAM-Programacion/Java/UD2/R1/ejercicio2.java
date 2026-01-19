49f46f8f55d079192f9310fa4bd65a45af7d2f26
ferrestorresantoniomiguel <ferrestorresantoniomiguel001.alu@stemgranada.com>
2026-01-19 15:46:03 +0100

    - ejercicio2 Ud2 R1 - ej3 Ud2 R1

---
+++ 1DAM-Programacion/Java/UD2/R1/ejercicio2.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class ejercicio2 {
+    public static void main(String[] args) {
+        
+        //Creamos el array de números enteros
+        ArrayList<String> colores = new ArrayList<>();
+        colores.add("rojo");
+        colores.add("verde");
+        colores.add("azul");
+
+        System.out.println("Estoy son mis colores: " + colores);
+    }
+}
---
+++ 1DAM-Programacion/Java/UD2/R1/ejercicio3.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class ejercicio3 {
+    public static void main(String[] args) {
+        
+        //Creamos el array de números enteros
+        ArrayList<String> colores = new ArrayList<>();
+        colores.add("rojo");
+        colores.add("verde");
+        colores.add("azul");
+
+        System.out.println("Estoy son mis colores: " + colores);
+        String colorBorrado = colores.remove(0);
+
+        System.out.println("Número eliminado: " + colorBorrado);
+        System.out.println("Colores restantes: " + colores);
+    }
+}
