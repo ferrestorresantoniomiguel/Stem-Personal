3202d60e2024a2ef772ead74db003a69c75fb772
ferrestorresantoniomiguel <ferrestorresantoniomiguel001.alu@stemgranada.com>
2026-02-01 11:56:03 +0100

    - haciendo pt1

--- 1DAM-Programacion/Java/PT1/pt1_NUEVO.java
+++ 1DAM-Programacion/Java/PT1/pt1_NUEVO.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+
 public class pt1_NUEVO {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final int STOCKMADERAINICIAL;
+        final double STOCKACEROINICIAL;
+        int numeroEncargos;
+        int numeroUnidades;
+        final int MADERAPORFLECHA = 1;
+        final int MADERAPORESCUDO = 3;
+        final int MADERAPORESPADA = 1;
+        final double ACEROPORFLECHA = 0.05;
+        final double ACEROPORESCUDO = 1.5;
+        final double ACEROPORESPADA = 1.2;
+        int maderaTotalNecesaria;
+        double aceroTotalNecesario;
+        int maderaDisponible;
+        double aceroDisponible;
+        
+    }
 }
